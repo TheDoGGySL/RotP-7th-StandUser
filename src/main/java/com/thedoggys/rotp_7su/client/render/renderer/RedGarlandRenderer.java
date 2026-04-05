ff1ce4f92df51bedfe1b8e9b32df881241a48c3d
Yarost228 <yarik.alferov1@gmail.com>
2026-04-05 21:37:40 +0300

    Fixed skins issue

--- src/main/java/com/thedoggys/rotp_7su/client/render/renderer/RedGarlandRenderer.java
+++ src/main/java/com/thedoggys/rotp_7su/client/render/renderer/RedGarlandRenderer.java
@@ -3,15 +3,18 @@ package com.thedoggys.rotp_7su.client.render.renderer;
 import com.github.standobyte.jojo.client.render.entity.model.stand.StandEntityModel;
 import com.github.standobyte.jojo.client.render.entity.model.stand.StandModelRegistry;
 import com.github.standobyte.jojo.client.render.entity.renderer.stand.StandEntityRenderer;
+import com.github.standobyte.jojo.client.standskin.StandSkinsManager;
 import com.thedoggys.rotp_7su.AddonMain;
 import com.thedoggys.rotp_7su.client.render.model.RedGarlandModel;
 import com.thedoggys.rotp_7su.entity.RedGarlandEntity;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
+import java.util.Optional;
+
 public class RedGarlandRenderer extends StandEntityRenderer<RedGarlandEntity, StandEntityModel<RedGarlandEntity>> {
     private static final ResourceLocation[] TEXTURES = new ResourceLocation[7];
-    private static final ResourceLocation[] TEXTURES_SKIN2 = new ResourceLocation[7];
+//    private static final ResourceLocation[] TEXTURES_SKIN2 = new ResourceLocation[7];
 
     static {
         for (int i = 0; i < 7; i++) {
@@ -19,10 +22,10 @@ public class RedGarlandRenderer extends StandEntityRenderer<RedGarlandEntity, St
                     AddonMain.MOD_ID,
                     "textures/entity/stand/red_garland" + (i + 1) + ".png"
             );
-            TEXTURES_SKIN2[i] = new ResourceLocation(
-                    AddonMain.MOD_ID,
-                    "textures/entity/stand/red_garland_" + (i + 1) + ".png"
-            );
+//            TEXTURES_SKIN2[i] = new ResourceLocation(
+//                    AddonMain.MOD_ID,
+//                    "textures/entity/stand/red_garland_" + (i + 1) + ".png"
+//            );
         }
     }
 
@@ -41,12 +44,12 @@ public class RedGarlandRenderer extends StandEntityRenderer<RedGarlandEntity, St
     public ResourceLocation getTextureLocation(RedGarlandEntity entity) {
         if (entity != null) {
             int frame = (entity.tickCount / 5) % 7;
-            if (entity.getStandSkin().equals("rotp_7su:skinc")) {
-                return TEXTURES_SKIN2[frame];
+            if (entity.getStandSkin().isPresent()) {
+                return StandSkinsManager.getInstance()
+                        .getRemappedResPath(manager -> manager.getStandSkin(entity.getStandSkin()), TEXTURES[frame]);
             }
             return TEXTURES[frame];
         }
         return TEXTURES[0];
     }
-    //todo:fix skin
 }
